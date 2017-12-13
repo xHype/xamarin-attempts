3148043cd07a396dc1cfea9a594ab3973d2d810f
xHype <radumazilu24@gmail.com>
2017-12-13 11:57:08 +0200

    Initial commit

---
+++ SVGCompatibility/SVGCompatibility.Droid/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace SVGCompatibility.Droid
+{
+    [Activity(Label = "SVGCompatibility.Droid", MainLauncher = true, Theme = "@style/Theme.AppCompat.NoActionBar")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
